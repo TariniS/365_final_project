93789e2d0f787d4db3d72aa5b9015fb108413fd7
TariniS <tareene@gmail.com>
2023-05-04 11:53:05 -0700

    finished add recipes endpoint

--- src/api/recipes.py
+++ src/api/recipes.py
@@ -1,4 +1,5 @@
 import sqlalchemy
+from pydantic import BaseModel
 from sqlalchemy import func
 from fastapi import APIRouter, HTTPException
 from enum import Enum
@@ -6,6 +7,8 @@ from src import database as db
 from typing import List
 
 router = APIRouter()
+
+
 @router.get("/recipes/{id}", tags=["recipes"])
 def get_recipe(id: str):
     """
@@ -28,10 +31,8 @@ def get_recipe(id: str):
     return id
 
 
-
 @router.get("/recipes/", tags=["recipes"])
 def get_recipes_by_ingredients(ingredient_list: str):
-
     """
     - This endpoint allows a user to search for recipes based on ingredients they have available on hand and returns
     the recipes in order of highest ingredient match
@@ -75,9 +76,8 @@ class recipe_sort_options(str, Enum):
 # Add get parameters
 @router.get("/recipes/", tags=["movies"])
 def list_recipes(
-    tag: str = "",
-    sort: recipe_sort_options = recipe_sort_options.rating):
-
+        tag: str = "",
+        sort: recipe_sort_options = recipe_sort_options.rating):
     """
     currently only supports filtering by one tag?
     This endpoint allows a user to search for recipes based on tags, and sort/filter based on rating.
@@ -98,5 +98,101 @@ def list_recipes(
     return None
 
 
+class Ingredient(BaseModel):
+    ingredient_name: str
+    core_ingredient: str
+    quantity: int
+    measurement: str
 
 
+class Instruction(BaseModel):
+    step_order: int
+    step_name: str
+
+
+class RecipeJson(BaseModel):
+    recipe_name: str
+    total_time: str
+    servings: int
+    spice_level: int
+    cooking_level: int
+    recipe_description: str
+    ingredients: List[Ingredient]
+    instructions: List[Instruction]
+
+
+@router.post("/recipes/{user_id}/recipe/", tags=["movies"])
+def add_recipe(user_id: int, recipe: RecipeJson):
+    existing_user_query = """ SELECT * 
+                              FROM users 
+                              WHERE users.user_id = :user_id"""
+    existing_user = db.conn.execute(sqlalchemy.text(existing_user_query), {'user_id': user_id})
+    last_recipe_id = """ SELECT recipe.recipe_id
+                              FROM recipe
+                              ORDER BY recipe_id DESC"""
+    last_ingredient_id = """ SELECT ingredients.ingredient_id
+                              FROM ingredients
+                              ORDER BY ingredient_id DESC"""
+    last_instruction_id = """ SELECT instructions.instruction_id
+                              FROM instructions
+                              ORDER BY instruction_id DESC"""
+    new_recipe_id = int(db.conn.execute(sqlalchemy.text(last_recipe_id)).first()[0]) + 1
+    new_ingredient_id = int(db.conn.execute(sqlalchemy.text(last_ingredient_id)).first()[0]) + 1
+    new_instruction_id = int(db.conn.execute(sqlalchemy.text(last_instruction_id)).first()[0]) + 1
+    # Error checking
+    count = 0
+    for row in existing_user:
+        count += 1
+    if count == 0:
+        raise HTTPException(status_code=404, detail="user_id not found. Please create a new user.")
+    # user_id valid
+    else:
+        with db.engine.begin() as conn:
+            conn.execute(
+                sqlalchemy.insert(db.recipes),
+                [
+                    {
+                        "recipe_id": new_recipe_id,
+                        "recipe_name": recipe.recipe_name,
+                        "user_id": user_id,
+                        "total_time": recipe.total_time,
+                        "servings": recipe.servings,
+                        "spicelevel": recipe.spice_level,
+                        "cookinglevel": recipe.cooking_level,
+                        "recipe_description": recipe.recipe_description
+                    }
+                ],
+            )
+
+            for i in range(len(recipe.ingredients)):
+                currentIngredient = recipe.ingredients[i]
+                current_ingredient_id = new_ingredient_id + i
+                conn.execute(
+                    sqlalchemy.insert(db.ingredients),
+                    [
+                        {
+                            "recipe_id": new_recipe_id,
+                            "ingredient_id": current_ingredient_id,
+                            "ingredient_name": currentIngredient.ingredient_name,
+                            "core_ingredient": currentIngredient.core_ingredient,
+                            "quantity": currentIngredient.quantity,
+                            "measurement": currentIngredient.measurement
+                        }
+                    ],
+                )
+            for i in range(len(recipe.instructions)):
+                currentInstruction = recipe.instructions[i]
+                current_instruction_id = new_instruction_id + i
+                conn.execute(
+                    sqlalchemy.insert(db.instructions),
+                    [
+                        {
+                            "instruction_id": current_instruction_id,
+                            "recipe_id": new_recipe_id,
+                            "step_order": currentInstruction.step_order,
+                            "step_name": currentInstruction.step_name
+                        }
+                    ],
+                )
+
+    return new_recipe_id
