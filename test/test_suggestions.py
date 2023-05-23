15df315c9836b847de4ae0573fb0f72e4da9423d
sameera <sameerabali5@gmail.com>
2023-05-22 21:51:05 -0700

    tests for pairings

--- test/test_suggestions.py
+++ test/test_suggestions.py
@@ -6,11 +6,10 @@ import sqlalchemy
 client = TestClient(app)
 
 def test_recipe_pairing_suggestions():
-    # Test case 1: invalid recipe pairing
-    response = client.get("/recipes/1/pairings")
-    assert response.status_code == 400
-    print(response.json())
-    assert "pairing_suggestions" not in response.json()
+    # Test case 1: Valid recipe pairing
+    response = client.get("/recipes/8/pairings")
+    assert response.status_code == 200
+    assert "pairing_suggestions" in response.json()
 
 def test_recipe_pairing_suggestions_2():
     # Test case 2: Invalid recipe_id
